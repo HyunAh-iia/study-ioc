841e7f3122800718b32ba6bc2e0ceaea578fcc61
iia <kha2464@gmail.com>
2021-05-09 15:09:12 +0900

    [Step3] DI Pattern : Constructor Injection

--- src/my/study/CustomerBusinessLogic.java
+++ src/my/study/CustomerBusinessLogic.java
@@ -3,8 +3,12 @@ package my.study;
 public class CustomerBusinessLogic {
 	private ICustomerDataAccess customerDataAccess;
 
+	public CustomerBusinessLogic(ICustomerDataAccess customerDataAccess) {
+		this.customerDataAccess = customerDataAccess;
+	}
+
 	public CustomerBusinessLogic() {
-		customerDataAccess = DataAccessFactory.getDataAccessObject();
+		customerDataAccess = new CustomerDataAccess();
 	}
 
 	public String getCustomerName(int id) {
